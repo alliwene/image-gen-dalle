58654533f1cc957a95ccd10a3739673b8ce14c66
Abiodun Allison <allisonabiodun@gmail.com>
2023-07-06 22:53:03 +0100

    feat: add slider for number of images

--- app.py
+++ app.py
@@ -12,10 +12,13 @@ with gr.Blocks() as demo:
                 show_label=False, placeholder="Please enter a prompt message"
             )
             button = gr.Button(value="Generate images")
+        slider = gr.Slider(
+            minimum=0, maximum=6, step=1, label="Number of images to generate", value=1
+        )
         gallery = gr.Gallery(
             height="auto",
             object_fit="contain",
             show_label=False,
         )
-        
-    button.click(fn=get_images, inputs=[textbox], outputs=gallery)
+
+    button.click(fn=get_images, inputs=[textbox, slider], outputs=gallery)
