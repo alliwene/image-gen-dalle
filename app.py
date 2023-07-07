16430bf15d52c26fabd1154c465c20e95f92b754
Abiodun Allison <allisonabiodun@gmail.com>
2023-07-07 23:32:35 +0100

    feat: add image variation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 
-from backend import get_images
+from backend import get_images, create_image_variation
 
 # inputs: textbox, button,
 # output: gallery
@@ -15,10 +15,20 @@ with gr.Blocks() as demo:
         slider = gr.Slider(
             minimum=0, maximum=6, step=1, label="Number of images to generate", value=1
         )
+
+        image = gr.Image(shape=(80, 80), source="upload", type="filepath")
+        image_button = gr.Button(value="Create variations from image")
+
         gallery = gr.Gallery(
             height="auto",
             object_fit="contain",
             show_label=False,
         )
 
+    image_button.click(fn=create_image_variation, inputs=[image, slider], outputs=gallery)
+
     button.click(fn=get_images, inputs=[textbox, slider], outputs=gallery)
+
+
+if __name__ == "__main__":
+    demo.launch()
--- backend.py
+++ backend.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 from typing import Literal, List
 
 import openai
+from openai import InvalidRequestError
 import requests
 from PIL.Image import Image
 from PIL import Image as img
 
 load_dotenv()
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def get_images(
@@ -18,8 +19,27 @@ def get_images(
     image_size: Literal["256x256", "512x512", "1024x1024"] = "256x256",
 ) -> List[Image]:
     response = openai.Image.create(prompt=prompt, n=num_of_images, size=image_size)
-    urls = [res["url"] for res in response['data']]
+    urls = [res["url"] for res in response["data"]]
     images = [img.open(requests.get(url, stream=True).raw) for url in urls]
-    
+
     return images
-    
+
+
+def create_image_variation(
+    img_path: str,
+    num_of_images: int = 1,
+    image_size: Literal["256x256", "512x512", "1024x1024"] = "256x256",
+):
+    img_mode = img.open(img_path).mode
+    if img_mode not in ["RGB", "RGBA"]:
+        raise InvalidRequestError(
+            f"Invalid input image - format must be RGB or RGBA not {img_mode}"
+        )
+
+    response = openai.Image.create_variation(
+        image=open(img_path, "rb"), n=num_of_images, size=image_size
+    )
+    urls = [res["url"] for res in response["data"]]
+    images = [img.open(requests.get(url, stream=True).raw) for url in urls]
+
+    return images
