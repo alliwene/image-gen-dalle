049f5d9097e058eaa4a9265958d6d938ecfefe0f
Abiodun Allison <allisonabiodun@gmail.com>
2023-07-10 21:58:55 +0100

    chore: revision

--- app.py
+++ app.py
@@ -8,26 +8,34 @@ from backend import get_images, create_image_variation
 with gr.Blocks() as demo:
     with gr.Column():
         with gr.Row():
+            # prompt textbox
             textbox = gr.Textbox(
                 show_label=False, placeholder="Please enter a prompt message"
             )
-            button = gr.Button(value="Generate images")
+            # generate image button 
+            img_gen_button = gr.Button(value="Generate images")
+        # slider
         slider = gr.Slider(
-            minimum=0, maximum=6, step=1, label="Number of images to generate", value=1
+            minimum=1, maximum=6, step=1, label="Number of images to generate", value=1
         )
 
-        image = gr.Image(shape=(80, 80), source="upload", type="filepath")
-        image_button = gr.Button(value="Create variations from image")
+        # upload image for creating image variations 
+        image = gr.Image(shape=(128, 128), source="upload", type="filepath")
+        # create image variation button 
+        img_variation_button = gr.Button(value="Create variations from image")
 
+        # gallery output 
         gallery = gr.Gallery(
             height="auto",
             object_fit="contain",
             show_label=False,
         )
 
-    image_button.click(fn=create_image_variation, inputs=[image, slider], outputs=gallery)
-
-    button.click(fn=get_images, inputs=[textbox, slider], outputs=gallery)
+    # generate image
+    img_gen_button.click(fn=get_images, inputs=[textbox, slider], outputs=gallery)
+    
+    # create image variation 
+    img_variation_button.click(fn=create_image_variation, inputs=[image, slider], outputs=gallery)
 
 
 if __name__ == "__main__":
